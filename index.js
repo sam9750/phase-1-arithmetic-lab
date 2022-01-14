53f73c13eed0a4d2916ae4a92e52db02f6e027a7
sam9750 <sam.wadhwa95@gmail.com>
2022-01-14 13:21:17 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+const num1 = 62;
+const num2 = 1;
+const multiply = num1*num2;
+console.log(multiply);
 
-// Write your code here
+
+
+
+
+
+function randomSolution() {
+return Math.floor(Math.random() * 10) + 1;
+}
+
+
+const random = Math.floor(Math.random() * 10) + 1
+
+
+
+const num3 = 24;
+const num4 = 5;
+const mod =num3 % num4;
+console.log(mod);
+
+
+
+const max = Math.max(5,20);
+
